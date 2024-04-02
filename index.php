14735c7e17c0045e45953732733b9f67fd07717f
SivaKeerthiyadav <104550682+SivaKeerthiyadav@users.noreply.github.com>
2024-04-02 16:23:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<?php
+  include("session.php");
+  $exp_category_dc = mysqli_query($con, "SELECT expensecategory FROM expenses WHERE user_id = '$userid' GROUP BY expensecategory");
+  $exp_amt_dc = mysqli_query($con, "SELECT SUM(expense) FROM expenses WHERE user_id = '$userid' GROUP BY expensecategory");
+
+  $exp_date_line = mysqli_query($con, "SELECT expensedate FROM expenses WHERE user_id = '$userid' GROUP BY expensedate");
+  $exp_amt_line = mysqli_query($con, "SELECT SUM(expense) FROM expenses WHERE user_id = '$userid' GROUP BY expensedate");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Expense Manager - Dashboard</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Feather JS for Icons -->
+  <script src="js/feather.min.js"></script>
+  <style>
+    .card a {
+      color: #000;
+      font-weight: 500;
+    }
+
+    .card a:hover {
+      color: #28a745;
+      text-decoration: dotted;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="border-right" id="sidebar-wrapper">
+      <div class="user">
+        <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="120">
+        <h5><?php echo $username ?></h5>
+        <p><?php echo $useremail ?></p>
+      </div>
+      <div class="sidebar-heading">Management</div>
+      <div class="list-group list-group-flush">
+        <a href="index.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="home"></span> Dashboard</a>
+        <a href="add_expense.php" class="list-group-item list-group-item-action "><span data-feather="plus-square"></span> Add Expenses</a>
+        <a href="manage_expense.php" class="list-group-item list-group-item-action sidebar-active"><span>&#8377;</span> Manage Expenses</a>
+      </div>
+      <div class="sidebar-heading">Settings </div>
+      <div class="list-group list-group-flush">
+        <a href="profile.php" class="list-group-item list-group-item-action "><span data-feather="user"></span> Profile</a>
+        <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> Logout</a>
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg navbar-light  border-bottom">
+
+
+        <button class="toggler" type="button" id="menu-toggle" aria-expanded="false">
+          <span data-feather="menu"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="25">
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="profile.php">Your Profile</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="logout.php">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+        <h3 class="mt-4">Dashboard</h3>
+        <div class="row">
+          <div class="col-md">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col text-center">
+                    <a href="add_expense.php"><img src="icon/addex.png" width="57px" />
+                      <p>Add Expenses</p>
+                    </a>
+                  </div>
+                  <div class="col text-center">
+                    <a href="manage_expense.php"><img src="icon/maex.png" width="57px" />
+                      <p>Manage Expenses</p>
+                    </a>
+                  </div>
+                  <div class="col text-center">
+                    <a href="profile.php"><img src="icon/prof.png" width="57px" />
+                      <p>User Profile</p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      
+     
+
+ <div class="container-fluid">
+        <h3 class="mt-4 text-center">Full-Expense Report</h3>
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr class="text-center">
+                            <th>S.I</th>
+                            <th>Date</th>
+                            <th>Amount</th>
+                            <th>Expense Category</th>
+                        </tr>
+                    </thead>
+                    <?php 
+                    $count = 1;
+                    $total_expenses = 0;
+                    $exp_fetched = mysqli_query($con, "SELECT * FROM expenses WHERE user_id = '$userid'");
+                    while ($row = mysqli_fetch_array($exp_fetched)) {
+                        $total_expenses += $row['expense'];
+                    ?>
+                    <tr>
+                        <td><?php echo $count; ?></td>
+                        <td> <?php echo $row['expensedate']; ?></td>
+                        <td><?php echo '₹ ' . $row['expense']; ?></td>
+                        <td><?php echo $row['expensecategory']; ?></td>
+                    </tr>
+                    <?php 
+                    $count++;
+                    } 
+                    ?>
+                    <tr>
+                        <td colspan="2" class="text-right"><strong>Total:</strong></td>
+                        <td colspan="2"><?php echo '₹ ' . $total_expenses; ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+      </div>
+    </div>   
+    <!-- /#page-content-wrapper -->
+  </div>
+  <!-- /#wrapper --> 
+
+  </script> 
+</body>
+
+</html>
